e2ba37b87b7d370116f1c3e7da1ce334a5f5393d
Rob McKinnon <@digital.justice.gov.uk>
2014-01-28 09:48:44 +0000

    secret_key_base now an env var; set to default value when in test or development environment

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,9 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-CivilClaimsFrontendDefendant::Application.config.secret_key_base = '41b50b49856db7e72748795a85ba80abd2bc03b13b3f16c4ce6dbb897f1ab7b6b14f86abf98924018de0c36623e65ffecd190a2658623b42959f1fd532d8139f'
+
+if ENV['SECRET_KEY_BASE'].nil? && (Rails.env.test? || Rails.env.development?)
+  ENV['SECRET_KEY_BASE'] = '41b50b49856db7e72748795a85ba80abd2bc03b13b3f16c4ce6dbb897f1ab7b6b14f86abf98924018de0c36623e65ffecd190a2658623b42959f1fd532d8139f'
+end
+
+CivilClaimsFrontendDefendant::Application.config.secret_key_base = ENV['SECRET_KEY_BASE']
